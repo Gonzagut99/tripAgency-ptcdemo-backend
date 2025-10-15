8d67904bec10aeeb489ef245dac285091ce4de51
Gonzalo Gutiérrez Castillo <gonzalogut.99@gmail.com>
2025-10-15 07:51:38 -0500

    Initial commit - Spring Boot project setup

---
+++ src/main/java/com/tripagency/ptc/ptcagencydemo/PtcagencydemoApplication.java
@@ -0,0 +1,13 @@
+package com.tripagency.ptc.ptcagencydemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PtcagencydemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PtcagencydemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tripagency/ptc/ptcagencydemo/PtcagencydemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tripagency.ptc.ptcagencydemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PtcagencydemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
